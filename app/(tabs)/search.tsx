52afc08827e6c69984b79cae7e2df57cd1a7737c
Sifan23 <sifanyg123@gmail.com>
2025-09-11 05:34:43 +0300

    feat : setting the search page to dynamic func

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,15 +4,33 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchPopularMovies } from '@/services/api'
 import useFetch from '@/services/useFetch'
-import { useRouter } from 'expo-router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 
 const Search = () => {
-  const router = useRouter();
-  const { data: movies, error, loading} = useFetch(() => fetchPopularMovies({
-    query: ''
-  }))
+  const [searchQuery, setSearchQuery ] = useState('');
+  const { 
+    data: movies, 
+    error, 
+    loading,
+    refetch: loadMovies,
+    reset
+  } = useFetch(() => fetchPopularMovies({
+    query: searchQuery
+  }), false)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+    if(searchQuery.trim()){
+      await loadMovies
+    } else {
+      reset()
+    }
+  },500)
+
+  return () => clearTimeout(timeoutId);
+  }, [searchQuery])
+  
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className='flex-1 w-full absoulte z-0' resizeMode='cover' />
@@ -34,15 +52,21 @@ const Search = () => {
           <Image source={icons.logo} className='w-12 h-10'/>
         </View>
         <View className='my-5'>
-          <SearchBar  placeholder='search movies...'/>
+          <SearchBar  
+          placeholder='search movies...'
+          value={searchQuery}
+          onChangeText={(text : string) => setSearchQuery(text)}
+          />
         </View>
         {loading && (
           <ActivityIndicator size="large" color="#0000ff" className='my-3'/>
         )}
         {error && (<Text className='text-red-500 px-5 my-3'>Error: {error?.message}</Text>)}
-        {!loading && !error && "Search Term".trim() && movies?.length > 0 && (
+        {
+        !loading && !error && searchQuery.trim()
+         && movies?.length > 0 && (
           <Text className='text-xl text-white font-bold'> Search Results for {' '}
-          <Text className='text-accent'>SEARCH TERM</Text>
+          <Text className='text-accent'>{searchQuery}</Text>
           </Text>
         )}
         </>
